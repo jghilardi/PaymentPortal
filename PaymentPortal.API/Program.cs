7c60159ac36715532390934ccb3a9e7e1289c201
jghilardi <jghilardi@gmail.com>
2025-10-11 13:24:02 -0600

    cleanup using

--- PaymentPortal.API/Program.cs
+++ PaymentPortal.API/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using PaymentPortal.Data;
